e17e02c620f939d62e7b526e7bdd7cbc85d7c914
Sohel <88754456+IRISHREX@users.noreply.github.com>
2024-04-03 17:31:19 +0530

    Update Ticket.js
    Unique key resolved

--- models/Ticket.js
+++ models/Ticket.js
@@ -1,8 +1,12 @@
 const mongoose = require('mongoose');
-
+//unique key added for all data.
 const ticketSchema = new mongoose.Schema({
     ticketCode: {
         type: String,
+    },
+    uid:{
+        type: String,
+        default:()=>new Date().getTime();
     }
 });
 
